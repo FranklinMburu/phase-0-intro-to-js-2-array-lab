b05582dc545ffb26402f3805edff54de95844097
FranklinMburuDev <184284364+FranklinMburu@users.noreply.github.com>
2025-01-30 23:58:19 +0300

    Implemented both destructive and non-destructive array manipulation functions for the cats array to pass test cases. Added methods for appending, prepending, and removing cats at both ends of the array, with respect to mutation rules.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+// Initializing the cats array
+var cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive methods:
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Non-destructive methods:
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
